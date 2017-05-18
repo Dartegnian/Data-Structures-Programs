f28110de3c9dcf93466b346910238e5f757aae4d
Dartegnian <dartegnian@users.noreply.github.com>
2017-05-18 13:45:08 +0800

    Minor changes

--- Infix and postfix calculator/DASTRUCFIX.java
+++ Infix and postfix calculator/DASTRUCFIX.java
@@ -1,44 +1,41 @@
 import javax.swing.JOptionPane;
 import java.util.Stack;
 
-public class DASTRUCFIX{
-    public static void main(String[] args){
-        Stack<String> rene = new Stack<String>();
-        DASTRUCOP operations = new DASTRUCOP();
-        Object[] choices = {"Store infix", "Show infix", "Show postfix", "Convert infix to postfix", "Clear storage", "Close"};
+public class DASTRUCFIX {
+    public static void main(String[] args) {
         String decision;
-        for(;;){
+        DASTRUCOP operations = new DASTRUCOP();
+        Stack<String> rene = new Stack<String>();
+        Object[] choices = {"Store infix", "Show infix", "Show postfix", "Convert infix to postfix", "Clear storage", "Close"};
+        
+        for (;;) {
             decision = (String) JOptionPane.showInputDialog(null, "Please choose", "Choices", JOptionPane.PLAIN_MESSAGE, null, choices, "Infix & Postfix");
-            if (decision == "Store infix"){
+
+            if (decision.equals("Store infix")) {
                 DASTRUCOP.storeInfix();
                 JOptionPane.showMessageDialog(null, "Infix stored!", "Success", JOptionPane.INFORMATION_MESSAGE);
             }
-            else if (decision == "Show infix"){
-                if(DASTRUCOP.infixStoreIsEmpty()){
+            else if (decision.equals("Show infix")) {
+                if (DASTRUCOP.infixStoreIsEmpty())
                     JOptionPane.showMessageDialog(null, "No infixes are stored", "None", JOptionPane.INFORMATION_MESSAGE);
-                }
-                else{
+                else
                     JOptionPane.showMessageDialog(null, DASTRUCOP.showInfix(), "Success", JOptionPane.INFORMATION_MESSAGE);
-                }
             }
-            else if (decision == "Show postfix"){
-                if(DASTRUCOP.postfixStoreIsEmpty()){
+            else if (decision.equals("Show postfix")) {
+                if (DASTRUCOP.postfixStoreIsEmpty())
                     JOptionPane.showMessageDialog(null, "No postfixes are stored", "None", JOptionPane.INFORMATION_MESSAGE);
-                }
-                else{
+                else
                     JOptionPane.showMessageDialog(null, DASTRUCOP.showPostfix(), "Success", JOptionPane.INFORMATION_MESSAGE);
-                }
             }
-            else if (decision == "Convert infix to postfix"){
+            else if (decision.equals("Convert infix to postfix")) {
                 DASTRUCOP.convertToPostfix();
             }
-            else if (decision == "Clear storage"){
+            else if (decision.equals("Clear storage")) {
                 DASTRUCOP.clearFixStore();
-                if(DASTRUCOP.clearFixStore()){
+                if (DASTRUCOP.clearFixStore())
                     JOptionPane.showMessageDialog(null, "Storage cleared!", "Success", JOptionPane.INFORMATION_MESSAGE);
-                }
             }
-            else if (decision == "Close"){
+            else if (decision.equals("Close")) {
                 System.exit(0);
             }
         }
