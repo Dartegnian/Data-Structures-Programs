8ebd355f86184124e4b6d62587fa9cbd30ba995f
Dartegnian <dartegnian@users.noreply.github.com>
2017-05-18 13:49:01 +0800

    Minor code changes and other stuff

--- Infix and postfix calculator/DASTRUCOP.java
+++ Infix and postfix calculator/DASTRUCOP.java
@@ -2,16 +2,16 @@ import javax.swing.JOptionPane;
 import java.util.LinkedList;
 import java.util.Stack;
 
-public class DASTRUCOP{
+public class DASTRUCOP {
+    private static String infix;
     private static LinkedList <String> infixStore = new LinkedList<String>();
     private static LinkedList <String> postfixStore = new LinkedList<String>();
-    private static String infix;
 
-    private static boolean isOperator(char c){
+    private static boolean isOperator(char c) {
         return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '(' || c == ')';
     }
 
-    private static boolean isLowerPrecedence(char op1, char op2){
+    private static boolean isLowerPrecedence(char op1, char op2) {
         switch (op1) {
             case '+':
             case '-':
@@ -31,26 +31,26 @@ public class DASTRUCOP{
                 return false;
         }
     }
-    public static String convertToPostfix(String infix){
+    public static String convertToPostfix(String infix) {
+        char c;
         Stack<Character> stack = new Stack<Character>();
         StringBuffer postfix = new StringBuffer(infix.length());
-        char c;
 
-        for(int i = 0; i < infix.length(); i++){
+        for (int i = 0; i < infix.length(); i++) {
             c = infix.charAt(i);
 
             if (!isOperator(c)) postfix.append(c);
-            else{
-                if(c == ')'){
-                    while (!stack.isEmpty() && stack.peek() != '('){
+            else {
+                if (c == ')') {
+                    while (!stack.isEmpty() && stack.peek() != '(') {
                         postfix.append(stack.pop());
                     }
-                    if(!stack.isEmpty())stack.pop();
+                    if (!stack.isEmpty())stack.pop();
                 }
-                else{
-                    if(!stack.isEmpty() && !isLowerPrecedence(c, stack.peek())) stack.push(c);
+                else {
+                    if (!stack.isEmpty() && !isLowerPrecedence(c, stack.peek())) stack.push(c);
                     else {
-                        while(!stack.isEmpty() && isLowerPrecedence(c, stack.peek())){
+                        while (!stack.isEmpty() && isLowerPrecedence(c, stack.peek())) {
                             Character pop = stack.pop();
                             if (c != '(') postfix.append(pop);
                             else c = pop;
@@ -65,37 +65,36 @@ public class DASTRUCOP{
         }
         return postfix.toString();
     }
-    public static String storeInfix(){
+    public static String storeInfix() {
         infix = JOptionPane.showInputDialog("What do you want to add?");
         infixStore.add(infix);
         return null;
     }
-    public static String showInfix(){
+    public static String showInfix() {
         return infixStore.get(0);
     }
-    public static String showPostfix(){
+    public static String showPostfix() {
         return postfixStore.get(0);
     }
-    public static String convertToPostfix(){
-        if(infixStore.isEmpty()){
-            JOptionPane.showMessageDialog(null, "No infixes are stored", "Empty", JOptionPane.INFORMATION_MESSAGE);
-        }
-        else{
+    public static String convertToPostfix() {
+        if (infixStore.isEmpty())
+            JOptionPane.showMessageDialog(null, "No infixes are stored.", "Empty", JOptionPane.INFORMATION_MESSAGE);
+        else {
             infix = infixStore.get(0);
             postfixStore.add(convertToPostfix(infixStore.get(0)));
             JOptionPane.showMessageDialog(null, "Infix converted!", "Success", JOptionPane.INFORMATION_MESSAGE);
         }
         return null;
     }
-    public static boolean infixStoreIsEmpty(){
+    public static boolean infixStoreIsEmpty() {
         return infixStore.isEmpty();
     }
-    public static boolean clearFixStore(){
+    public static boolean clearFixStore() {
         infixStore.clear();
         postfixStore.clear();
         return infixStore.isEmpty() && postfixStore.isEmpty();
     }
-    public static boolean postfixStoreIsEmpty(){
+    public static boolean postfixStoreIsEmpty() {
         return postfixStore.isEmpty();
     }
 }
